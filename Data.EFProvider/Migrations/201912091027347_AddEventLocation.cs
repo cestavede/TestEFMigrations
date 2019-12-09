6f0bacc3748c36b2450c98618d24055fb93299b0
Bohdan Blahitka <bohdan.b.blahitka@pwc.com>
2019-12-09 11:29:50 +0100

    Add-Migration, Update-Database

---
+++ Data.EFProvider/Migrations/201912091027347_AddEventLocation.cs
@@ -0,0 +1,18 @@
+﻿namespace Data.EFProvider.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddEventLocation : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CalendarEvents", "Location", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CalendarEvents", "Location");
+        }
+    }
+}
